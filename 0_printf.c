3e82a9e24a1ec3d324cb81eaef1d1cfcc8e1e6c9
jusiam <160097231+jusiam@users.noreply.github.com>
2024-04-23 10:45:36 +0300

    Update 0_printf.c

--- 0_printf.c
+++ 0_printf.c
@@ -9,48 +9,48 @@
  */
 int _printf(const char *format, ...)
 {
-va_list args;                                    
+va_list args;
 int count = 0;
 const char *ptr = format;
 int (*func_p)(va_list) = NULL;
 if (format == NULL)
-    return (-1);
-    va_start(args, format);                          /* Start var */
-    while (*ptr)
-    {
-        if (*ptr != '%')
-        {
-            _putchar(*ptr);                          /* Print the char */
-            count++;                                 /* Increment char count */
-        }
-        else                                         /* If current char is '%' */
-        {
-            ptr++;                                   /* Move to next char */
-            if (*ptr == '\0')                        /* If next char is NULL */
-                return (-1);                         /* Return -1 */
-            func_p = get_func(*ptr);                 /* Get function pointer */
-            if (func_p != NULL)
-                count += func_p(args);               /* Call function and update count */
-            else
-            {
-                if (*ptr == 's')                     /* If specifier is 's' */
-                    count += print_string(args);
-                else if (*ptr == 'c')
-                    count += print_character(args);
-                else if (*ptr == '%')                /* If specifier is '%' */
-                    count += print_percent(args);
-                else if (*ptr == 'd' || *ptr == 'i') /* If specifier is 'd' or 'i' */
-                    count += print_integer(args);
-                else                                 /* If unknown specifier */
-                {
-                    _putchar('%');                   /* Print '%' */
-                    _putchar(*ptr);                  /* Print specifier char */
-                    count += 2;                      /* Increment count by 2 */
-                }
-            }
-        }
-        ptr++;                                       /* Move to next char in format str */
-    }
-    va_end(args);                                    /* End */
-    return (count);
+return (-1);
+va_start(args, format);                          /* Start var */
+while (*ptr)
+{
+if (*ptr != '%')
+{
+_putchar(*ptr);                          /* Print the char */
+count++;                                 /* Increment char count */
+}
+else                                         /* If current char is '%' */
+{
+ptr++;                                   /* Move to next char */
+if (*ptr == '\0')                        /* If next char is NULL */
+return (-1);                         /* Return -1 */
+func_p = get_func(*ptr);                 /* Get function pointer */
+if (func_p != NULL)
+count += func_p(args);               /* Call function and update count */
+else
+{
+if (*ptr == 's')                     /* If specifier is 's' */
+count += print_string(args);
+else if (*ptr == 'c')
+count += print_character(args);
+else if (*ptr == '%')                /* If specifier is '%' */
+count += print_percent(args);
+else if (*ptr == 'd' || *ptr == 'i') /* If specifier is 'd' or 'i' */
+count += print_integer(args);
+else                                 /* If unknown specifier */
+{
+_putchar('%');                   /* Print '%' */
+_putchar(*ptr);                  /* Print specifier char */
+count += 2;                      /* Increment count by 2 */
+}
+}
+}
+ptr++;                                       /* Move to next char in format str */
+}
+va_end(args);                                    /* End */
+return (count);
 }
