5dc5875007882b08ca95337095dfaebd02cb6cef
jusiam <160097231+jusiam@users.noreply.github.com>
2024-04-23 11:10:53 +0300

    Update 0_printf.c

--- 0_printf.c
+++ 0_printf.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdarg.h>
-
 /**
  * _printf - create printf
  * @format: The format str
@@ -8,8 +7,7 @@
  * Return: The number of char printed
  */
 int _printf(const char *format, ...)
-{
-va_list args;
+{va_list args;
 int count = 0;
 const char *ptr = format;
 int (*func_p)(va_list) = NULL;
@@ -17,23 +15,18 @@ if (format == NULL)
 return (-1);
 va_start(args, format);                          /* Start var */
 while (*ptr)
-{
-if (*ptr != '%')
-{
-_putchar(*ptr);                          /* Print the char */
-count++;                                 /* Increment char count */
-}
-else                                         /* If current char is '%' */
-{
-ptr++;                                   /* Move to next char */
+{if (*ptr != '%')
+{_putchar(*ptr);                          /* Print the char */
+count++;}
+else
+{ptr++;                                   /* Move to next char */
 if (*ptr == '\0')                        /* If next char is NULL */
 return (-1);                         /* Return -1 */
 func_p = get_func(*ptr);                 /* Get function pointer */
 if (func_p != NULL)
 count += func_p(args);               /* Call function and update count */
 else
-{
-if (*ptr == 's')                     /* If specifier is 's' */
+{if (*ptr == 's')                     /* If specifier is 's' */
 count += print_string(args);
 else if (*ptr == 'c')
 count += print_character(args);
@@ -42,15 +35,9 @@ count += print_percent(args);
 else if (*ptr == 'd' || *ptr == 'i') /* If specifier is 'd' or 'i' */
 count += print_integer(args);
 else                                 /* If unknown specifier */
-{
-_putchar('%');                   /* Print '%' */
+{_putchar('%');                   /* Print '%' */
 _putchar(*ptr);                  /* Print specifier char */
-count += 2;                      /* Increment count by 2 */
-}
-}
-}
-ptr++;
-}
+count += 2;}}}
+ptr++;}
 va_end(args);
-return (count);
-}
+return (count);}
